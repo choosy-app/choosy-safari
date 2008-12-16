428c4ea596de2e0cc0259872adfe7aa1687624b8
George Brocklehurst <george.brocklehurst@gmail.com>
2008-12-16 17:05:52 +0000

    Added readme file and license information

--- BrowserWebView.h
+++ BrowserWebView.h
@@ -1,2 +1,10 @@
+//
+//  BrowserWebView.h
+//  ChoosySafari
+//
+//  Created by George on 16/12/2008.
+//  Copyright 2008 George Brocklehurst. Some rights reserved (see accompanying LICENSE file for details).
+//
+
 @interface BrowserWebView : NSObject
 @end
